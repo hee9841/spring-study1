156390c54601cbe2bdb330b6c5676d486bbd216d
hee9841 <lee98417716@gmail.com>
2023-06-15 15:53:36 +0900

    세션4 - 컨트롤러 생성

---
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -0,0 +1,19 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * packageName    : hello.hellospring.controller
+ * fileName       : MemberController
+ * author         : asdfz
+ * date           : 2023-06-15
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023-06-15        asdfz       최초 생성
+ */
+@Controller
+public class MemberController {
+
+}
